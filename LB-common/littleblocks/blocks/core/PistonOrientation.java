08a3a56c516590393ad4e61f0cddaf0b5c81f49b
Eurymachus <reflex_ion@hotmail.com>
2012-12-02 03:36:26 +0000

    PistonOrientation Cleanup

--- LB-common/littleblocks/blocks/core/PistonOrientation.java
+++ LB-common/littleblocks/blocks/core/PistonOrientation.java
@@ -1,15 +1,29 @@
 package littleblocks.blocks.core;
 
+import net.minecraft.src.World;
+import net.minecraftforge.event.ForgeSubscribe;
+
 // import net.minecraftforge.event.entity.block.BlockOrientationEvent;
 
 public class PistonOrientation {
 
-	/*
-	 * @ForgeSubscribe public void onPistonPlacement(BlockOrientationEvent
-	 * event) { LittleBlockCoordinates coords = new LittleBlockCoordinates(
-	 * event.blockX, event.blockY, event.blockZ); if
-	 * (LittleBlockDataHandler.isLittleWorld(event.world)) {
-	 * LittleBlockDataHandler.shiftCoordsRight(coords, 3); event.blockX =
-	 * coords.x; event.blockY = coords.y; event.blockZ = coords.z; } }
-	 */
+	// @ForgeSubscribe
+	public void onPistonPlacement(/* BlockOrientationEvent event */) {
+		int x, y, z;
+		x = 0;/* event.blockX; */
+		y = 0;/* event.blockY; */
+		z = 0;/* event.blockZ; */
+		LittleBlockCoordinates coords = new LittleBlockCoordinates(x, y, z);
+		World world;
+		world = null;/* event.world; */
+		if (LittleBlockDataHandler.isLittleWorld(world)) {
+			LittleBlockDataHandler.shiftCoordsRight(coords, 3);
+			x = coords.x;
+			y = coords.y;
+			z = coords.z;
+		}
+		// event.blockX = x;
+		// event.blockY = y;
+		// event.blockZ = z;
+	}
 }
