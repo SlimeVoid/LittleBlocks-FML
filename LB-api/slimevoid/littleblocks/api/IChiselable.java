767055b2c44f25c345f9ffa9bc943e71a5261db7
Eurymachus <reflex_ion@hotmail.com>
2013-01-19 13:33:11 +0000

    Moved API to own Repo

--- LB-api/slimevoid/littleblocks/api/IChiselable.java
+++
@@ -1,7 +0,0 @@
-package slimevoid.littleblocks.api;
-
-public interface IChiselable {
-	public boolean isChiselable();
-	
-	public int getChiseledAmount();
-}
--- LB-source/slimevoid/littleblocks/core/LBInit.java
+++ LB-source/slimevoid/littleblocks/core/LBInit.java
@@ -3,6 +3,7 @@ package slimevoid.littleblocks.core;
 import slimevoid.lib.ICommonProxy;
 import slimevoid.lib.ICore;
 import slimevoid.lib.core.Core;
+import slimevoid.littleblocks.api.util.LittleBlocksHelper;
 
 public class LBInit {
 	public static ICore LBM;
@@ -15,6 +16,7 @@ public class LBInit {
 		LBM = new Core(proxy);
 		LBM.setModName("LittleBlocks");
 		LBM.setModChannel("LITTLEBLOCKS");
+		LittleBlocksHelper.init(proxy);
 	}
 
 	public static void load() {
