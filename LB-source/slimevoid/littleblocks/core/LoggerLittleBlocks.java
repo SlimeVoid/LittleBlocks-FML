717620284db19de8c1bd610baf3223c4e19d9179
Eurymachus <reflex_ion@hotmail.com>
2013-04-10 14:43:59 +0100

    Small Logger change

--- LB-source/slimevoid/littleblocks/core/LoggerLittleBlocks.java
+++ LB-source/slimevoid/littleblocks/core/LoggerLittleBlocks.java
@@ -4,7 +4,7 @@ import slimevoid.lib.data.Logger;
 
 public class LoggerLittleBlocks extends Logger {
 	
-	private static Logger instance;
+	private static LoggerLittleBlocks instance;
 
 	@Override
 	protected String getLoggerName() {
@@ -12,11 +12,12 @@ public class LoggerLittleBlocks extends Logger {
 	}
 	
 	public static Logger getInstance(String name) {
-		if (instance == null)
+		if (instance == null) {
 			instance = new LoggerLittleBlocks();
+		}
 
 		instance.setName(name);
-
+		
 		return instance;
 	}
 }
