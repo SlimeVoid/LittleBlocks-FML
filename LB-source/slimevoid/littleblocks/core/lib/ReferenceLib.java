9176d6c33177585b35538db6cf9773e905bcf7c6
Eurymachus <reflex_ion@hotmail.com>
2013-04-27 16:14:26 +0100

    Version update

--- LB-source/slimevoid/littleblocks/core/lib/ReferenceLib.java
+++ LB-source/slimevoid/littleblocks/core/lib/ReferenceLib.java
@@ -4,7 +4,7 @@ public class ReferenceLib {
 	
 	public static final String MOD_ID = "LittleBlocksMod";
 	public static final String MOD_NAME = "Little Blocks";
-	public static final String MOD_VERSION = "2.0.2.1";
+	public static final String MOD_VERSION = "2.1.0.0";
 	public static final String MOD_DEPENDENCIES = "after:SlimevoidLib";
 	public static final String MOD_CHANNEL = "LITTLEBLOCKS";
 	public static final String CLIENT_PROXY = "slimevoid.littleblocks.client.proxy.ClientProxy";
