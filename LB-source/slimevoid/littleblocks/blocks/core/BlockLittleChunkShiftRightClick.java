52bad89205ccb9c8c80d7b7696b7d27ccb4e41e0
Eurymachus <reflex_ion@hotmail.com>
2013-05-29 00:20:14 +0100

    Fix duplication bug on shift right click to place

--- LB-source/slimevoid/littleblocks/blocks/core/BlockLittleChunkShiftRightClick.java
+++ LB-source/slimevoid/littleblocks/blocks/core/BlockLittleChunkShiftRightClick.java
@@ -24,7 +24,7 @@ public class BlockLittleChunkShiftRightClick {
 				if (FMLCommonHandler.instance().getSide() == Side.CLIENT) {
 					Block.blocksList[LBCore.littleChunkID].onBlockActivated(world, x, y, z, entityplayer, event.face, 0, 0, 0);
 				}
-				event.setResult(Result.DENY);
+				event.setCanceled(true);
 			}
 		}
 	}
