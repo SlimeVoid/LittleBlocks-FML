7b9dea4be252c15192239cb8e357e8b5da8d6d64
Eurymachus <reflex_ion@hotmail.com>
2013-07-11 11:45:29 +0100

    isLadder addition (Not currently working)

--- LB-source/slimevoid/littleblocks/blocks/BlockLittleChunk.java
+++ LB-source/slimevoid/littleblocks/blocks/BlockLittleChunk.java
@@ -15,6 +15,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.Item;
@@ -24,6 +25,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.src.ModLoader;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.MathHelper;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.util.Vec3;
 import net.minecraft.world.IBlockAccess;
@@ -75,6 +77,98 @@ public class BlockLittleChunk extends BlockContainer {
 	@Override
 	public int getRenderBlockPass() {
 		return 1;
+	}
+	
+	/**
+	 * Retrieves whether or not the block the entity has collided with is a Ladder
+	 * (Mainly used in Gulliver)
+	 * 
+	 * Credits : Tarig and Unclemion
+	 */
+	@Override
+    public boolean isLadder(World world, int x, int y, int z, EntityLivingBase entity) {
+		
+		TileEntityLittleChunk tile = (TileEntityLittleChunk) world
+				.getBlockTileEntity(x, y, z);
+		
+		int minXHit = entity.boundingBox.minX + 0.001D < x ? 0 : MathHelper
+				.floor_double((entity.boundingBox.minX + 0.001D)
+				* (double) tile.size)
+				% tile.size;
+		
+		int yHit = MathHelper.floor_double(entity.boundingBox.minY
+				* (double) tile.size)
+				% tile.size;
+		
+		int minZHit = entity.boundingBox.minZ + 0.001D < z ? 0 : MathHelper
+				.floor_double((entity.boundingBox.minZ + 0.001D)
+				* (double) tile.size)
+				% tile.size;
+		
+		int maxXHit = entity.boundingBox.maxX - 0.001D > (x + 1) ? 7
+				: MathHelper.floor_double((entity.boundingBox.maxX - 0.001D)
+				* (double) tile.size)
+				% tile.size;
+		
+		int maxZHit = entity.boundingBox.maxZ - 0.001D > (z + 1) ? 7
+				: MathHelper.floor_double((entity.boundingBox.maxZ - 0.001D)
+				* (double) tile.size)
+				% tile.size;
+		
+		boolean result = false;
+		if (!cpw.mods.fml.common.Loader.isModLoaded("GulliverForged")
+				|| (entity.boundingBox.maxY - entity.boundingBox.minY <= 1)) {
+			//System.out.println("isVanillaLadder");
+			int blockid = 0;
+			for (int xx = minXHit; xx <= maxXHit; xx++) {
+				blockid = tile.getBlockID(xx, yHit, minZHit);
+
+				if (blockid > 0) {
+
+					//System.out.println("isMinZLadder(" + xx + ", " + yHit + ", " + minZHit + ", " + entity.getEntityName());
+					result = Block.blocksList[blockid].isLadder(
+							(World) tile.getLittleWorld(), xx,
+							yHit, minZHit, entity);
+					if (result)
+						break;
+				}
+				blockid = tile.getBlockID(xx, yHit, maxZHit);
+				if (blockid > 0) {
+					//System.out.println("isMaxZLadder(" + xx + ", " + yHit + ", " + minZHit + ", " + entity.getEntityName());
+					result = Block.blocksList[blockid].isLadder(
+							(World) tile.getLittleWorld(), xx,
+							yHit, maxZHit, entity);
+					if (result)
+						break;
+				}
+			}
+
+			if (!result) {
+
+				for (int zz = minZHit; zz <= maxZHit; zz++) {
+					blockid = tile.getBlockID(minXHit, yHit, zz);
+					if (blockid > 0) {
+						//System.out.println("isMinXLadder(" + minXHit + ", " + yHit + ", " + zz + ", " + entity.getEntityName());
+						result = Block.blocksList[blockid].isLadder(
+								(World) tile.getLittleWorld(), minXHit,
+								yHit, zz, entity);
+						if (result)
+							break;
+					}
+					blockid = tile.getBlockID(maxXHit, yHit, zz);
+					if (blockid > 0) {
+						//System.out.println("isMinXLadder(" + maxXHit + ", " + yHit + ", " + zz + ", " + entity.getEntityName());
+						result = Block.blocksList[blockid].isLadder(
+								(World) tile.getLittleWorld(), maxXHit,
+								yHit, zz, entity);
+						if (result)
+							break;
+					}
+				}
+			}
+		}
+
+		return result;
 	}
 
 	@Override
