4310e776ce36fbd860ba8a713885048439e9132d
Allen Cook <alcook16@gmail.com>
2013-08-18 09:42:25 -0400

    Added required dependency
    this will show an error message for users when they don't install slimevoidlib

--- LB-source/slimevoid/littleblocks/core/lib/CoreLib.java
+++ LB-source/slimevoid/littleblocks/core/lib/CoreLib.java
@@ -6,7 +6,7 @@ public class CoreLib {
 	public static final String MOD_RESOURCES = "littleblocks";
 	public static final String MOD_NAME = "Little Blocks";
 	public static final String MOD_VERSION = "2.2.0.0";
-	public static final String MOD_DEPENDENCIES = "after:SlimevoidLib";
+	public static final String MOD_DEPENDENCIES = "required-after:SlimevoidLib";
 	public static final String MOD_CHANNEL = "LITTLEBLOCKS";
 	public static final String CLIENT_PROXY = "slimevoid.littleblocks.client.proxy.ClientProxy";
 	public static final String COMMON_PROXY = "slimevoid.littleblocks.proxy.CommonProxy";
