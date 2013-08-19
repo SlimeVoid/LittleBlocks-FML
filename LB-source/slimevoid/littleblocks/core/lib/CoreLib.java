c4e1dda8bf7c963453afb1a9026a1a18ad81a5ec
Eurymachus <reflex_ion@hotmail.com>
2013-08-19 23:10:37 +0100

    Removed Space

--- LB-source/slimevoid/littleblocks/core/lib/CoreLib.java
+++ LB-source/slimevoid/littleblocks/core/lib/CoreLib.java
@@ -14,7 +14,6 @@ public class CoreLib {
 	public static final String MOD_CHANNEL = "LITTLEBLOCKS";
 	public static final String CLIENT_PROXY = "slimevoid.littleblocks.client.proxy.ClientProxy";
 	public static final String COMMON_PROXY = "slimevoid.littleblocks.proxy.CommonProxy";
-	
 	@SideOnly(Side.CLIENT) 
 	public static boolean OPTIFINE_INSTALLED = FMLClientHandler.instance().hasOptifine();;
 }
