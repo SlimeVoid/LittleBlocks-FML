c2a97ec3595019ceaa613ffbf6a8894ff4690431
Eurymachus <reflex_ion@hotmail.com>
2013-09-17 22:04:44 +0100

    Version 2.2.1.0

--- LB-source/slimevoid/littleblocks/core/lib/CoreLib.java
+++ LB-source/slimevoid/littleblocks/core/lib/CoreLib.java
@@ -9,7 +9,7 @@ public class CoreLib {
 	public static final String MOD_ID = "LittleBlocks";
 	public static final String MOD_RESOURCES = "littleblocks";
 	public static final String MOD_NAME = "Little Blocks";
-	public static final String MOD_VERSION = "2.2.0.0";
+	public static final String MOD_VERSION = "2.2.1.0";
 	public static final String MOD_DEPENDENCIES = "after:SlimevoidLib";
 	public static final String MOD_CHANNEL = "LITTLEBLOCKS";
 	public static final String CLIENT_PROXY = "slimevoid.littleblocks.client.proxy.ClientProxy";
