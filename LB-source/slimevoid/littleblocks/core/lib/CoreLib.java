c4757162857a7a5b5e411a8fa6606ad9a3a974ad
Eurymachus <reflex_ion@hotmail.com>
2013-09-17 22:09:34 +0100

    Required-After Slimevoid Lib

--- LB-source/slimevoid/littleblocks/core/lib/CoreLib.java
+++ LB-source/slimevoid/littleblocks/core/lib/CoreLib.java
@@ -10,7 +10,7 @@ public class CoreLib {
 	public static final String MOD_RESOURCES = "littleblocks";
 	public static final String MOD_NAME = "Little Blocks";
 	public static final String MOD_VERSION = "2.2.1.0";
-	public static final String MOD_DEPENDENCIES = "after:SlimevoidLib";
+	public static final String MOD_DEPENDENCIES = "required-after:SlimevoidLib";
 	public static final String MOD_CHANNEL = "LITTLEBLOCKS";
 	public static final String CLIENT_PROXY = "slimevoid.littleblocks.client.proxy.ClientProxy";
 	public static final String COMMON_PROXY = "slimevoid.littleblocks.proxy.CommonProxy";
