a759020983674f4ccb0f42e74063ea3256372c88
Allen Cook <alcook16@gmail.com>
2014-07-08 19:58:40 -0400

    Fix apparent changes to depend string

--- src/main/java/net/slimevoid/littleblocks/core/lib/CoreLib.java
+++ src/main/java/net/slimevoid/littleblocks/core/lib/CoreLib.java
@@ -11,7 +11,7 @@ public class CoreLib {
     public static final String MOD_NAME            = "Little Blocks";
     public static final String MOD_VERSION         = "@VERSION@";
     public static final String MOD_DEPENDENCIES    = "";
-    public static final String ACTUAL_DEPENDENCIES = "required-after:Forge@[@FORGEVERSION@,),SlimevoidLib@[@SLIMEVERSION@,)";
+    public static final String ACTUAL_DEPENDENCIES = "required-after:Forge@[@FORGEVERSION@,);required-after:SlimevoidLib@[@SLIMEVERSION@,)";
     public static final String MOD_CHANNEL         = "LITTLEBLOCKS";
     public static final String CLIENT_PROXY        = "net.slimevoid.littleblocks.client.proxy.ClientProxy";
     public static final String COMMON_PROXY        = "net.slimevoid.littleblocks.proxy.CommonProxy";
